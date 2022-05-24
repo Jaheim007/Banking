90712db6eb69e313f93bf694bdd0e5e4a6ec196e
iMac-13 <imac-13@192.168.88.102>
2022-05-24 16:40:27 +0000

    learned the static and BASE_DIR with os.path.join()

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'pages/index.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+# Create your views here.
